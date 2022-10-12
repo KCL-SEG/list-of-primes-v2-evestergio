0664089e4595e8a678f76c7fbf6d1e0d8554b8b7
Eve Ster <paraskevi.stergiopoulou@kcl.ac.uk>
2022-10-12 23:14:05 +0100

    Exercise 1.4 completed

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes > 0:
+        num = 1
+        while len(list) < number_of_primes:
+            num = num + 1
+            if isPrime(num):
+                list.append(num)
+    else:
+        raise ValueError('The number of primes must be greater than 0.')
     return list
+
+def isPrime(num):
+    for i in range(2, num):
+        if num%i == 0:
+            return False
+    return True
